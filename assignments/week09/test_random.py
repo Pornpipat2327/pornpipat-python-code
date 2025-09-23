5b8d60643a8101d8872badc26a3a01437b746516
Pornpipat Kumlong <pornpipat.ku@ku.th>
2025-09-23 11:52:17 +0700

    Randon number

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,22 @@
 import random
 
 def test_random():
+    # สุ่มเลขระหว่าง 1 - 10 เก็บไว้ในตัวแปร random_number
     random_number = random.randint(1, 100)
+
+    # รับค่าการทายเลขจากผู้ใช้ เก็บไว้ในตัวแปร guess_number
+    guess_number = input("What is your guess number?: ")
+    guess_number = int(guess_number)
+
+    if random_number == guess_number:
+        print("Congratulations")
+
+    elif random_number < guess_number:
+        print("Too much")
+
+    elif random_number > guess_number:
+        print("Too low")
+
     print(random_number)
     
 test_random()
