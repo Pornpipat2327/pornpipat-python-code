1a98592904923d6454d47964109025bf35df0037
Pornpipat Kumlong <pornpipat.ku@ku.th>
2025-10-07 11:42:15 +0700

    KK

--- assignments/week11/temperature.py
+++ assignments/week11/temperature.py
@@ -22,4 +22,27 @@ Highest: 28 C
 Lowest: 19 C
 
 """
-    
+
+def get_temperatures():
+    x = [32, 30, 31, 33, 30, 31, 34]
+    return x 
+
+def analyze_temps(temp_list):
+    average_temp = 0
+    sum = 0
+    for temp in temp_list:
+        sum = sum + temp
+    average_temp = sum / 7    
+    max_temp = max(temp_list)
+    min_temp = min(temp_list)
+    return (average_temp, max_temp, min_temp)
+
+def display_analysis(avg, high, low):
+    print("Temperature Analysis for the Week")
+    print("Average: %.1f C" % (avg))
+    print(f"Highest: {hight} c")
+    print("Lowest: %d c" % (low))
+
+temp_list = get_temperatures()
+result = analyze_temps(temp_list)
+display_analysis(result[0], result[1], result[2])     
